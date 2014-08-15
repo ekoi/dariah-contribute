b729d470c8d2d0da80d24bc6bdb893ced0c61030
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2014-08-15 12:26:57 +0200

    Improve URLs.

--- dariah_contributions/urls.py
+++ dariah_contributions/urls.py
@@ -1,23 +1,23 @@
 from django.conf.urls import patterns, url
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from django.views.generic import RedirectView
 
 from .models import Contribution
 from .views import ContributionCreate, ContributionDelete, ContributionUpdate, ContributionRDF
 
 
 urlpatterns = patterns('',
-    # example: /dariah_contributions/
-    url(r'^$', RedirectView.as_view(url='list'), name='index'),
-    # example: /dariah_contributions/list/
-    url(r'^all/$', ListView.as_view(model=Contribution, queryset=Contribution.published.all()), name='list'),
-    # example: /dariah_contributions/detail/5/
+    url(r'^(all)?/$', ListView.as_view(model=Contribution, queryset=Contribution.published.all()), name='list'),
+    # example: /contribution/
+    # example: /contribution/all/
     url(r'^add/$', ContributionCreate.as_view(), name='add'),
+    # example: /contribution/add/
     url(r'^(?P<pk>\d+)/update/$', ContributionUpdate.as_view(), name='update'),
+    # example: /contribution/5/update/
     url(r'^(?P<pk>\d+)/delete/$', ContributionDelete.as_view(), name='delete'),
+    # example: /contribution/5/delete/
     url(r'^(?P<pk>\d+)\.xml$', ContributionRDF.as_view(), name='detail_rdf'),
+    # example: /contribution/detail_rdf/5/
     url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Contribution, queryset=Contribution.published.all()), name='detail'),
-    # example: /dariah_contributions/detail_rdf/5/
-
+    # example: /contribution/5/
 )
