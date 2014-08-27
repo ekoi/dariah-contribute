6fa62301762335762b5169430c800423f3e75d06
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2014-08-27 11:35:41 +0200

    Improve autocomplete on skos:prefLabel Technique.

--- dariah_static_data/autocomplete_light_registry.py
+++ dariah_static_data/autocomplete_light_registry.py
@@ -2,18 +2,24 @@ import autocomplete_light
 from .models import TADIRAHTechnique
 
 
-autocomplete_light.register(TADIRAHTechnique,
-    search_fields=['^name', ],  # Just like in ModelAdmin.search_fields
-    attrs={
+class TADIRAHTechniqueAutocomplete(autocomplete_light.AutocompleteModelBase):
+    search_fields = ['^name', ]
+    model = TADIRAHTechnique
+    attrs = {
         # This will set the input placeholder attribute:
-        'placeholder': 'Technique item...?',
+        'placeholder': 'Start typing...',
         # This will set the yourlabs.Autocomplete.minimumCharacters
         # options, the naming conversion is handled by jQuery
         'data-autocomplete-minimum-characters': 1,
-    },
-    widget_attrs={
-        'data-widget-maximum-values': 4,
-        # Enable modern-style widget !
-        'class': 'modern-style',
     }
-)
+    widget_attrs = {
+        #'data-widget-maximum-values': 4,
+        # Enable modern-style widget !
+        #'class': 'modern-style',
+    }
+
+
+"""Note: adding the model as the first argument is a temporary fix for
+https://github.com/yourlabs/django-autocomplete-light/issues/313.
+"""
+autocomplete_light.register(TADIRAHTechnique, TADIRAHTechniqueAutocomplete)
