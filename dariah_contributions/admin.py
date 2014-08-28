7b345e8d9f6d02a4ab9d62d654cd4d1bc5ec45bc
Heleen <heleen.van.de.schraaf@dans.knaw.nl>
2014-08-28 09:53:19 +0200

    Add more fields to the admin.

--- dariah_contributions/admin.py
+++ dariah_contributions/admin.py
@@ -3,8 +3,9 @@ from dariah_contributions.models import Contribution, DcCreator, DcContributor
 
 
 class ContributionAdmin(admin.ModelAdmin):
+    list_display = ['dc_identifier', 'dc_title', 'is_published', 'is_deleted']
     list_filter = ['published_on', ]
-    readonly_fields = ['author', 'last_modified_on', 'dc_identifier']
+    readonly_fields = ['author', 'last_modified_on', 'dc_identifier', 'is_deleted']
     filter_horizontal = ['dc_creator', 'dc_contributor']
 
 admin.site.register(DcCreator)
