5c8f12c884938b9d850bc88386bcfd12e87ee8c3
ekoi <eko.indarto@dans.knaw.nl>
2015-02-12 11:59:15 +0100

    Add required_css_class = 'required' to show *

--- dariah_annual_value/forms.py
+++ dariah_annual_value/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from .models import AnnualValue
 
 class AnnualValueForm(forms.ModelForm):
-   
+    required_css_class = 'required'
     #inkind = forms.ModelChoiceField(queryset=AnnualValue.objects.all(), empty_label="Choose one of my contributions below:", help_text="Inkind help text")
     justification = forms.CharField( widget=forms.Textarea, help_text="Help text for 'justification'- forms.py")
    
