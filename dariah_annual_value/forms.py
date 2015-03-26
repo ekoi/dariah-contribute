9d1d4dbe8ddabbc71503a5961519daf91d8271dd
ekoi <eko.indarto@dans.knaw.nl>
2015-03-26 11:50:54 +0100

    Cleaning up: remove unused code/imports

--- dariah_annual_value/forms.py
+++ dariah_annual_value/forms.py
@@ -4,10 +4,7 @@ from .models import AnnualValue
 
 class AnnualValueForm(forms.ModelForm):
     required_css_class = 'required'
-    #inkind = forms.ModelChoiceField(queryset=AnnualValue.objects.all(), empty_label="Choose one of my contributions below:", help_text="Inkind help text")
-    justification = forms.CharField( widget=forms.Textarea, help_text="Help text for 'justification'- forms.py")
-   
+    
     class Meta:
         model = AnnualValue
-        
     
